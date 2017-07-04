3a6fe61ca20c22aebadce9ecd3e2934532642808
DESKTOP-R7KEFO8\Paul Tanchareon <paul.vtan@live.com>
2017-07-05 02:01:21 +1200

    test delay

--- moby_bot/src/js/custom.js
+++ moby_bot/src/js/custom.js
@@ -5,19 +5,26 @@ var s12 = "sounds/12.mp3";
 
 
 $(document).ready(function () {
+    var scenario = 0;
     //This function plays a moby voice feedback.
     $("#user-text").keypress(function (e) {
         if (e.which == 13) {
             var user_command = $("#user-text").val().toLowerCase();
 
-            setTimeout(function () {
-                //Scenario 1
-                new Audio(s11).play();
-                if (user_command.includes("hi")) {
-                    setTimeout(function () {
-                        new Audio(s12).play();
-                    }, 3500);
-                }
+            //Scenario 1
+            //setTimeout(function () {
+            //    new Audio(s11).play();
+            //    if (user_command.includes("hi")) {
+            //        setTimeout(function () {
+            //            new Audio(s12).play();
+            //        }, 3500);
+            //    }
+
+            if (user_command.includes("hi")) {
+                setTimeout(function () {
+                    new Audio(s11).play();
+                }, 3500);
+            
                 //----------
 
                 //Scenario 2
