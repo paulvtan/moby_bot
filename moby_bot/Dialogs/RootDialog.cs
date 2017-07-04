5e58eaf3a8547a3183b1184c19c124f13c7be224
DESKTOP-R7KEFO8\Paul Tanchareon <paul.vtan@live.com>
2017-07-04 22:53:14 +1200

    test declearing card building method

--- moby_bot/Dialogs/RootDialog.cs
+++ moby_bot/Dialogs/RootDialog.cs
@@ -24,47 +24,42 @@ namespace moby_bot.Dialogs
             var activity = await result as Activity;
 
             //Call GetQnaReply to get the result
-            QnAMakerResult QnaMakeReply = QnaMaker.GetQnaReply(activity);
+            QnAMakerResult qnaMakeReply = QnaMaker.GetQnaReply(activity);
 
             //Reply to user
-            string QnaAnswer = QnaMakeReply.Answer;
-            await context.PostAsync(QnaAnswer.Substring(2, QnaAnswer.Length - 2));
-
-            //Test Adaptive Card
-            //var heroCard = new HeroCard
-            //{
-            //    Text = "Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.",
-            //    Buttons = new List<CardAction> { new CardAction("imBack", "Yes", value: "Yes"), new CardAction("imBack", "No", value: "No") }
-            //};
-
-            var heroCard = new ThumbnailCard
-            {
-                Title = "BotFramework Thumbnail Card",
-                Subtitle = "Your bots — wherever your users are talking",
-                Text = "Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.",
-                Images = new List<CardImage> { new CardImage("https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg") },
-                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Get Started", value: "https://docs.microsoft.com/bot-framework"), new CardAction(ActionTypes.OpenUrl, "Get Started", value: "https://docs.microsoft.com/bot-framework") }
-            };
-
-            Activity replyToConversation = activity.CreateReply();
-            // Create the attachment.
-            Attachment attachment = heroCard.ToAttachment();
-            replyToConversation.Attachments.Add(attachment);
-            await context.PostAsync(replyToConversation);
+            string qnaAnswer = qnaMakeReply.Answer;
+            await context.PostAsync(qnaAnswer.Substring(2, qnaAnswer.Length - 2));
             //-----------------------------------------------------
 
 
             //2nd Level Reply
-            string scenarioCode = QnaAnswer.Substring(0, 2);
+            string scenarioCode = qnaAnswer.Substring(0, 2);
             switch (scenarioCode)
             {
                 case "11":
                     Thread.Sleep(3000);
-                    await context.PostAsync("Would you like me to help you allocate this transaction to the right account?");
+                    Activity replyToUser = createCardActivity(activity, "Would you like me to help you allocate this transaction to the right account?", 11);
+                    await context.PostAsync(replyToUser);
                     break;
             }
 
             context.Wait(MessageReceivedAsync);
+        }
+
+
+        //Create Hero Yes and No card
+        public static Activity createCardActivity(Activity activity,string text, int scenarioNo)
+        {
+            var heroCard = new HeroCard
+            {
+                Text = text,
+                Buttons = new List<CardAction> { new CardAction("imBack", "Yes", value: "Yes"), new CardAction("imBack", "No", value: "No") }
+            };
+            Activity replyToConversation = activity.CreateReply();
+            // Create the attachment.
+            Attachment attachment = heroCard.ToAttachment();
+            replyToConversation.Attachments.Add(attachment);
+            return replyToConversation;
         }
     }
 }
