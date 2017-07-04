f759eb323dbaeb9b02d39cc9cbf5ac588abafe25
DESKTOP-R7KEFO8\Paul Tanchareon <paul.vtan@live.com>
2017-07-04 22:36:44 +1200

    test hero card

--- moby_bot/Dialogs/RootDialog.cs
+++ moby_bot/Dialogs/RootDialog.cs
@@ -33,11 +33,8 @@ namespace moby_bot.Dialogs
             //Test Adaptive Card
             var heroCard = new HeroCard
             {
-                Title = "BotFramework Hero Card",
-                Subtitle = "Your bots — wherever your users are talking",
                 Text = "Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.",
-                Images = new List<CardImage> { new CardImage("https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg") },
-                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Get Started", value: "https://docs.microsoft.com/bot-framework") }
+                Buttons = new List<CardAction> { new CardAction("imBack", "Yes", value: "Yes"), new CardAction("imBack", "No", value: "No") }
             };
 
             Activity replyToConversation = activity.CreateReply();
