80142870cea57290e4ab29be651df1c3116defcc
DESKTOP-R7KEFO8\Paul Tanchareon <paul.vtan@live.com>
2017-07-06 03:58:16 +1200

    Updated \n

--- moby_bot/Dialogs/RootDialog.cs
+++ moby_bot/Dialogs/RootDialog.cs
@@ -63,7 +63,7 @@ namespace moby_bot.Dialogs
                     break;
 
                 case "16":
-                    replyToUser = createCardActivity(activity, "Yes ", "No", "Sounds like this is an 'Electricity & Gas' expense. Would you like me to allocate this transaction to the 'Electricity & Gas' expense account?", 13);
+                    replyToUser = createCardActivity(activity, "Yes ", "No", "Sounds like this is an 'Electricity & Gas' expense.\n Would you like me to allocate this transaction to the 'Electricity & Gas' expense account?", 13);
                     await context.PostAsync(replyToUser);
                     break;
             }
